1f8501b47db3f06e54d2a6fe440a78c4177a5728
SilverBirchh <bradleybirch09@yahoo.co.uk>
2018-11-09 14:34:40 +0000

    Remove sys call

--- ocr.py
+++ ocr.py
@@ -3,7 +3,6 @@ import pytesseract
 import argparse
 import cv2
 import os
-import sys
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -12,31 +11,28 @@ ap.add_argument("-p", "--preprocess", type=str, default="thresh",
 	help="type of preprocessing to be done")
 args = vars(ap.parse_args())
 
-# load the example image and convert it to grayscale
+# Get the image and convert to greyscale to seperate back and fore ground
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
-# check to see if we should apply thresholding to preprocess the
-# image
+# should thresholding be applied
 if args["preprocess"] == "thresh":
 	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
  
-# make a check to see if median blurring should be done to remove
-# noise
+# should noise reduction be applied
 elif args["preprocess"] == "blur":
 	gray = cv2.medianBlur(gray, 3)
  
 # write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
+# load the image as a PIL/Pillow image, apply OCR, and then delete the file
 text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
+
+# print the text the OCR found
 print(text)
 
-sys.exit()
 exit()
